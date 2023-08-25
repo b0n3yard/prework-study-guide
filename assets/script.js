c35cc63e10eee03d94f0cdb9ee0ae3505c96b4ea
b0n3yard <namesake39@gmail.com>
2023-08-25 13:17:28 -0400

    added functions

--- assets/script.js
+++ assets/script.js
@@ -1,17 +1,25 @@
+
 const stuff = ['html', 'css','Git','Javascript'];
+for(var i = 0; i < stuff.length; i++) {
+    console.log(stuff[i]);
+
+}
 let choice  = Math.floor(Math.random() * 4);
 console.log(choice);
 
 var topics = stuff[choice];
-
-if (topics === 'html') {
-    console.log("let's study html");
-} else if (topics === 'css') {
-    console.log("let's study css");
-} else if (topics === 'Git') {
-    console.log("let's study Git");
-} else if (topics === 'Javascript') {
-    console.log("let's study Javascript");
-} else {
-console.log('please try again');
+function topicselect(){
+    if (topics === 'html') {
+        console.log("let's study html");
+    } else if (topics === 'css') {
+        console.log("let's study css");
+    } else if (topics === 'Git') {
+        console.log("let's study Git");
+    } else if (topics === 'Javascript') {
+        console.log("let's study Javascript");
+    } else {
+    console.log('please try again');
+    }
 }
+
+topicselect(topics);
